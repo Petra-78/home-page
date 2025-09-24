6ba3375bfbbc240a9cf202970ca04ea6c0fddca2
Petra <partos.petra.0305@gmail.com>
2025-09-24 17:24:11 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# webpack-template
+
+## Run:
+
+```
+npm install
+```
---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+import prettierPlugin from "eslint-plugin-prettier";
+import prettierConfig from "eslint-config-prettier";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: globals.browser,
+    },
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      "no-unused-vars": "warn",    
+      "no-console": "off",          
+    },
+  },
+  prettierConfig, 
+]);
+
