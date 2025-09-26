46eb25b041430c3afa4381dd119ad5cff3b9fd7c
Petra-78 <partos.petra.0305@gmail.com>
2025-09-26 13:35:04 +0200

    change  to ''

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,24 +1,23 @@
-import globals from "globals";
-import { defineConfig } from "eslint/config";
-import prettierPlugin from "eslint-plugin-prettier";
-import prettierConfig from "eslint-config-prettier";
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
 
 export default defineConfig([
   {
-    files: ["**/*.{js,mjs,cjs}"],
+    files: ['**/*.{js,mjs,cjs}'],
     languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
+      ecmaVersion: 'latest',
+      sourceType: 'module',
       globals: globals.browser,
     },
     plugins: {
       prettier: prettierPlugin,
     },
     rules: {
-      "no-unused-vars": "warn",    
-      "no-console": "off",          
+      'no-unused-vars': 'warn',
+      'no-console': 'off',
     },
   },
-  prettierConfig, 
+  prettierConfig,
 ]);
-
